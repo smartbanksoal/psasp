f95b10c7365f48d7d62de9e0176fcf0edbaf2dd1
smartbanksoal <endrastodwi79@gmail.com>
2026-05-04 21:26:27 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Nama cache versi (Naikkan versinya ke v2, v3, dst. jika nanti ada perubahan file)
-const CACHE_NAME = 'psasp-v3';
+const CACHE_NAME = 'psasp-v4';
 
 const urlsToCache = [
   './',
